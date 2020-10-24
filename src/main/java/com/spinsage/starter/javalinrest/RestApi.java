0687c7e47ab5fef6064d2ec780506de7dbc87b86
spinsager <shashi@spinsage.com>
2020-10-24 06:51:45 +0530

    added more api methoods.

--- src/main/java/com/spinsage/starter/javalinrest/RestApi.java
+++ src/main/java/com/spinsage/starter/javalinrest/RestApi.java
@@ -5,9 +5,11 @@ import io.javalin.Javalin;
 
 public class RestApi {
     public static void main(String[] args) {
-        ProductController productController =  new ProductController();
+        ProductController productController = new ProductController();
 
         Javalin app = Javalin.create().start(7000);
         app.get("/product", productController.getAllProducts);
+        app.get("/product/:id", productController.getById);
+        app.post("/product", productController.addProduct);
     }
 }
--- src/main/java/com/spinsage/starter/javalinrest/dao/ProductDao.java
+++ src/main/java/com/spinsage/starter/javalinrest/dao/ProductDao.java
@@ -9,6 +9,7 @@ public class ProductDao {
 
     private List<Product> products = new ArrayList<>();
     private static ProductDao dao;
+
     public static ProductDao instance() {
         if (dao == null) {
             dao = new ProductDao();
@@ -17,11 +18,26 @@ public class ProductDao {
     }
 
     public ProductDao() {
-        products.add(Product.builder().name("Test1").description("Desc1").amount(20.00).build());
-        products.add(Product.builder().name("Test2").description("Desc2").amount(10.00).build());
+        products.add(Product.builder().id(1L).name("Test1").description("Desc1").amount(20.00).build());
+        products.add(Product.builder().id(2L).name("Test2").description("Desc2").amount(10.00).build());
     }
 
     public List<Product> getAllProducts() {
         return products;
     }
+
+    public Product getById(Long id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product addProduct(Product product) {
+        products.add(product);
+        return product;
+    }
+
 }
